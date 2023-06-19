0530d09e731ec9921de01268dc7960be891a1097
AllLook <alet5289@gmail.com>
2023-06-20 01:42:23 +0300

    Seminar4

---
+++ venv/ATM_operation.py
@@ -0,0 +1,64 @@
+# Вроде все условия задачи))
+import sys
+from datetime import datetime
+SUM_ACTION = 50
+res_amount = 0
+operation_sheet = []
+
+
+def withdrawal(SUM_ACTION):
+    global res_amount
+    if res_amount < SUM_ACTION:
+        print("Недостаточно средств!")
+        sys.exit()
+
+    else:
+        res_amount = res_amount - SUM_ACTION
+        percent = SUM_ACTION // 100 * 1.5
+        if percent < 30:
+            percent = 30
+        elif percent > 600:
+            percent = 600
+        res_amount = res_amount - percent
+    print(f"остаток по счету: {res_amount}")
+
+    operation_sheet.append(
+        f"{datetime.now()} Текущая операция сняти на сумму {SUM_ACTION} со снятием процента {percent}, остаток по счету {res_amount} ")
+    return res_amount
+
+
+def top_up(SUM_ACTION):
+    global res_amount
+    res_amount = res_amount + SUM_ACTION
+    percent = SUM_ACTION // 100 * 1.5
+    if percent < 30:
+        percent = 30
+    elif percent > 600:
+        percent = 600
+    res_amount = res_amount - percent
+    print(res_amount)
+    print(f"остаток по счету: {res_amount}")
+    operation_sheet.append(
+        f"{datetime.now()} Текущая операция снятия на сумму {SUM_ACTION} со снятием процента {percent}, остаток по счету {res_amount} ")
+    return res_amount
+
+
+run = True
+while run:
+    if res_amount > 5000000:
+        percent = res_amount // 100 * 10
+        res_amount -= percent
+        print(
+            f"Вычтен налог на богатство в размере 10% ваш остаток {res_amount}")
+    action = input(
+        "Введите действие: пополнить,снять,выйти,стоп\nчтобы вывести список операций:список \n").lower()
+    if action == "снять":
+        withdrawal(SUM_ACTION)
+    elif action == "пополнить":
+        top_up(SUM_ACTION)
+    elif action == "список":
+        print(operation_sheet)
+    else:
+        action == "стоп"
+        run = False 
+        print("До свидания!")
---
+++ venv/transpose_matrix.py
@@ -0,0 +1,17 @@
+# matrix = [[1,5,9,7], [8,0,6,3],[9,8,6,5]]
+# def mat(matrix:list):
+#     matrix2 = []#новая матрица
+#     for i in range(len(matrix[0])):#длинна одного списка первой матрицы
+#         matrix2.append(list())#столько списков будет в результирующей матрице по длинне одного списка первой матрицы
+#         for j  in range(len(matrix)):#по кол-ву списков первой матрицы(будет строк в новой)
+#             matrix2[i].append(matrix[j][i])#на текущий элемент одного из списков второй матрицы добавить элемент из первой матрицы
+#     return matrix2
+
+# print(mat(matrix))
+
+matrix = iter([[1, 5, 9, 7], [8, 0, 6, 3], [9, 8, 6, 5]])
+matrix2 = zip(next(matrix), next(matrix), next(matrix))
+# matrix2 = list(zip(list(next(matrix)),list(next(matrix)),list(next(matrix))))#так выдает лист кортежей,почему сразу нельзя применить и перевести так в список списков?
+res_matrix = list(map(list, matrix2))
+print(res_matrix)
+# print(matrix2) 
